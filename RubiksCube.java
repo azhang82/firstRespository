d0c34deabc0607d238576aa433cb14c679319ef1
azhang81 <azzhang2020@gmail.com>
2022-05-10 17:52:06 -0400

    i love jews

---
+++ RubiksCube.java
@@ -0,0 +1,13 @@
+public class RubiksCube {
+    public int height = 56;
+    public float width = 56;
+
+    public void turn() {
+        System.out.println("turn");
+
+    }
+    public void stateHeight() {
+        System.out.println(this.height);
+    }
+
+}
